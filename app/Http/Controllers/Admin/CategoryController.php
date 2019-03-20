3398c8f27096300bab8dbc49468e1eda61b23255
Danial Abbas <danialabbas646@gmail.com>
2019-03-20 17:09:03 +0200

    backend 2

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Requests\CategoryRequest;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        $categories = Category::all();
+        return view('admin.categories.index',compact('categories'));
+    }
+    public function create()
+    {
+        return view('admin.categories.create-edit');
+    }
+    public function edit($id)
+    {
+        $category = Category::findOrFail($id);
+        return view('admin.categories.create-edit',compact('category'));
+    }
+    public function store(CategoryRequest $request)
+    {
+        $this->validate($request,['image_path'=>'image|required']);
+        $category = new Category();
+        if ($request->hasFile('image_path'))
+        {
+            $category->image_path = $request->file('image_path')->store('categories','public');
+        }
+        $category->title = $request->get('title');
+        $category->description = $request->get('description');
+        $category->save();
+
+        return redirect(action('Admin\CategoryController@index'))->with('success','Category Added');
+
+    }
+    public function update(CategoryRequest $request,$id)
+    {
+        $category = Category::findOrFail($id);
+        if ($request->hasFile('image_path'))
+        {
+            if (file_exists(storage_path('/storage/'.$category->image_path)))
+            {
+                unlink(storage_path('/storage/'.$category->image_path));
+            }
+            else{
+                $category->image_path = $request->file('image_path')->store('categories','public');
+            }
+        }
+        $category->title = $request->get('title');
+        $category->description = $request->get('description');
+        $category->save();
+
+        return redirect(action('Admin\CategoryController@index'))->with('success','Category Updated');
+
+    }
+    public function destroy($id)
+    {
+        $category = Category::findOrFail($id);
+        if (file_exists(storage_path('/storage/'.$category->image_path)))
+        {
+            unlink(storage_path('/storage/'.$category->image_path));
+        }
+
+        $category->delete();
+        return redirect(action('Admin\CategoryController@index'))->with('success','Category Deleted..');
+    }
+}
