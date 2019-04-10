6c53f4bc310d015d5a894f468236424ab6441891
Danial Abbas <danialabbas646@gmail.com>
2019-04-10 11:10:12 +0300

    backend 9.5

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -19,7 +19,7 @@
 <div class="preloader">
     <div class="loader">
         <div class="loader__figure"></div>
-        <p class="loader__label">DirectGroup</p>
+        <p class="loader__label">Creativo</p>
     </div>
 </div>
 <!-- ============================================================== -->
